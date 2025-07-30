b7aef922e48122fc0838e0c06d5820f497a523e7
madinator3 <13380187-madinator3@users.noreply.gitlab.com>
2025-07-30 10:45:57 -0400

    importing modules and list of elements

--- scripts/00_configs.py
+++ scripts/00_configs.py
@@ -6,8 +6,29 @@
 #=============== Libraries ==============
 import os
 import sys
+import time
+import logging
+from pathlib import Path
 import yaml
 import pandas as pd
+from Bio import Entrez
+
 
 
 #=============== Importing modules internal to the project ==============
+from scripts.utils import save_data_to_file
+
+
+#=============== List of elements to export ==============================
+# Lista de elementos exportados
+__all__ = [
+    "os",
+    "sys",
+    "time",
+    "logging",
+    "Path",
+    "yaml",
+    "pd",
+    "Entrez",
+    "save_data_to_file"
+]
