46bc06ab2f0a825475008f27a5ed2cce8575dfbf
madinator3 <13380187-madinator3@users.noreply.gitlab.com>
2025-07-30 10:41:09 -0400

    AI sucks files recovered

---
+++ scripts/01_querysetup.py
@@ -0,0 +1,26 @@
+# Title: Defining query for PubMed Metadata Extraction
+# Last Updated: 2025-07-23
+# Description: This script imports necessary libraries for extracting PubMed Metadata 
+# Source: https://github.com/TLDWTutorials/PubmedAPI/blob/main/pubmed_api_in_python_2024.py
+
+# Define lists of authors and topics
+authors = ['Bryan Holland', 'Mehmet Oz', 'Anthony Fauci']  # Example authors, adjust as needed
+topics = ['RNA', 'cardiovascular']  # Example topics, adjust as needed
+
+# Define date range
+date_range = '("2012/03/01"[Date - Create] : "2022/12/31"[Date - Create])'
+
+# Build the query dynamically based on the available authors and topics.
+queries = []
+
+if authors:
+    author_queries = ['{}[Author]'.format(author) for author in authors]
+    queries.append('(' + ' OR '.join(author_queries) + ')')
+
+if topics:
+    topic_queries = ['{}[Title/Abstract]'.format(topic) for topic in topics]
+    queries.append('(' + ' OR '.join(topic_queries) + ')')
+
+full_query = ' AND '.join(queries) + ' AND ' + date_range
+
+print(full_query)
---
+++ scripts/02_pubmedsearch.py
@@ -0,0 +1,10 @@
+# Title: Searching PubMed
+# Last Updated: 2025-07-30
+# Description: This script searches PubMed for articles related to a specific topic using the Entrez Programming Utilities (E-utilities).
+# Source: https://github.com/TLDWTutorials/PubmedAPI/blob/main/pubmed_api_in_python_2024.py
+
+#=============== Libraries ==============
+import pandas as pd
+import json
+from Bio import Entrez
+
