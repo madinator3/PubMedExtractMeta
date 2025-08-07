18a0c4c5ef238cfd4eebb11b031fb261dd4acc6f
madinator3 <madison3bell@gmail.com>
2025-08-07 10:11:49 -0400

    Trying to add in NIH icites query

--- scripts/pubmedsearch.py
+++ scripts/pubmedsearch.py
@@ -171,11 +171,70 @@ for pmid in id_list:
 
         df = pd.concat([df, new_row], ignore_index=True)
 
-print(grants)
 print(df)
 
-#TO DO: Change for loop to function, annotate function,  and number of citations extraction, and error handling
+import requests
 
+BASE_URL = "https://icite.od.nih.gov/api"
+field_list = ['pmid', 'year', 'title', 'apt', 'relative_citation_ratio', 'cited_by_clin']
+MAX_PMIDS_PER_REQUEST = 1000
+sleep = 0.05
+api_url = f'{BASE_URL}/pubs'
+
+
+def get_icites(self, pmid_list=[], field_list=[], timeout=500):
+    """
+    Fetches publication data in bulk. If pmid_list exceeds 1000, it splits the requests and merges the results.
+     Parameters:
+            pmid_list: List of PMIDs,
+            field_list: List of fields to be fetched,
+            timeout: Timeout for the request, default is 500 seconds.
+    Returns:
+    A JSON object containing merged results of the requests.
+    """
+    BASE_URL = "https://icite.od.nih.gov/api"
+    MAX_PMIDS_PER_REQUEST = 1000
+    sleep = 0.05
+    api_url = f'{BASE_URL}/pubs'
+
+    responses_data = []  # To store response data from all batches
+
+    # Split pmid_list into sublists of up to 1000 PMIDs each
+    for i in range(0, len(pmid_list), MAX_PMIDS_PER_REQUEST):
+        time.sleep(sleep)
+        sub_pmid_list = pmid_list[i:i + MAX_PMIDS_PER_REQUEST]
+        payload = {
+            'pmids': ','.join(map(str, sub_pmid_list)),
+            'fl': ','.join(field_list)
+        }
+
+        try:
+            response = requests.get(api_url, params=payload, timeout=timeout)
+            if response.status_code == 200:
+                # Add this batch's results to the list of response data
+                responses_data.extend(response.json().get('data', []))
+            else:
+                response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f"Request failed: {e}")
+            return None
+
+    # Merge results from all batches and return
+        
+    return {
+        'meta': {
+            'pmids': ','.join(map(str, pmid_list)),
+            'fl': ','.join(field_list)
+        },
+        'data': responses_data
+    }
+
+get_icites(id_list, field_list)
+
+print(id_list)
+ 
+
+#TO DO: Change for loop to function, annotate function,  and number of citations extraction
 #================ Export results =============================================================
 
 from scripts.utils import save_data_to_file
