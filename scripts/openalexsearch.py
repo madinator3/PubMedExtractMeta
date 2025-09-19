9506a0c9b48b5164dd05285c4201dd5840ba651d
madinator3 <13380187-madinator3@users.noreply.gitlab.com>
2025-09-19 10:39:12 -0400

    Parsing OA JSON...mixed success. json hates me

--- scripts/openalexsearch.py
+++ scripts/openalexsearch.py
@@ -81,11 +81,90 @@ while cursor is not None:
 print(len(ua_articles))
 
 # Convert the list of articles to a DataFrame
-df = pd.json_normalize(ua_articles) 
+# df = pd.json_normalize(ua_articles) 
 
+# DataFrame to store the extracted data
+df = pd.DataFrame(columns=['OAID', 'Title', 'Abstract', 'Authors','Author_ORCIDs','Affiliations',
+                           'Journal_Title', 'Journal_ISO', 'Keywords', 'Date_Pub_Year', 
+                           'Date_Pub_Month', 'Date_Pub_Day', 'Date_Revise_Year', 
+                           'Date_Revise_Month', 'Date_Revise_Day', 'Grants', 'URL', 'DOI'])
 
-#TO DO: parse the DataFrame to only include relevant columns; Add authors to query if possible
-#================ Export results =============================================================
+# Fetch information for each record in the ua_articles list
+for record in ua_articles:
+    # Print the record in a formatted JSON style
+    print(json.dumps(record, indent=4, default=str))  # default=str handles types JSON can't serialize like datetime
+
+    # Extract title information
+    title = record['title']
+
+    # Extract open alex id and url
+    url = record['id']
+    oaid = record['id']
+
+    # Extract doi
+    if 'doi' in record and record['doi'] is not None:
+        doi_url = record['doi']
+        doi = doi_url.replace("https://doi.org/", "")
+    else:
+        doi = "NONE"
+
+    # Extract author information
+    authors = []
+    authors_orcid = []
+    affiliations = []
+    affiliations_ror = []
+    affiliations_oaid = []
+    affiliations_country = []
+    if 'authorships' in record and record['authorships'] is not None: 
+        for author in record['authorships']['author']:
+            # Extract author names
+            if 'display_name' in author and author['display_name'] is not None:
+                name = author['display_name']
+            else: 
+                name = "NONE"
+            authors.append(name)
+
+            # Extract author ORCIDs
+            if 'orcid' in author and author['orcid'] is not None:
+                orcid = author['orcid'] 
+            else: 
+                orcid = "NONE"
+            authors_orcid.append(orcid)
+        authors = '; '.join(authors)
+        authors_orcid = '; '.join(authors_orcid)
+
+    else: 
+        authors = "NONE"
+            
+    
+ # Create a new row with the extracted data
+    new_row = pd.DataFrame({
+        'OAID': [oaid],
+        'Title': [title],
+            # 'Abstract': [abstract],
+        'Authors': [authors],
+        'Author_ORCIDs': [authors],
+            # 'Affiliations': [affiliations],
+            # 'Journal_Title': [journal_title],
+            # 'Journal_ISO': [journal_iso],
+            # 'Keywords': [keywords],
+            # 'Date_Pub_Year': [date_pub_yr],
+            # 'Date_Pub_Month': [date_pub_month],
+            # 'Date_Pub_Day': [date_pub_day],
+            # 'Date_Revise_Year': [date_revise_yr],
+            # 'Date_Revise_Month': [date_revise_month],
+            # 'Date_Revise_Day': [date_revise_day],
+            # 'Grants': [grants],
+        'URL': [url],
+        'DOI': [doi]
+        })
+
+    df = pd.concat([df, new_row], ignore_index=True)
+
+print(df)
+
+# #TO DO: parse the DataFrame to only include relevant columns; Add authors to query if possible
+# #================ Export results =============================================================
 
 from scripts.utils import save_data_to_file
 
